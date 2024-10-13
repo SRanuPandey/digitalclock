f8364a773182e95ef2d9b1d5bc1e648ee4892a74
1988Shubham <shubhampandey88@gmail.com>
2024-10-13 20:28:53 +0100

    unique plan file - 2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.5.0"
+      version = "~> 3.0"  
     }
   }
+  required_version = ">= 1.0"
 }
-
 provider "azurerm" {
   features {}
    resource_provider_registrations = "none"
