337187af160230f5aff420ef965201e5c79f09a9
1988Shubham <shubhampandey88@gmail.com>
2024-10-13 23:00:22 +0100

    unique plan file - 12

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_subnet" "appgw_subnet" {
   name                 = "appgw-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]  # Ensure this subnet doesn't overlap with others
+  address_prefixes     = ["10.0.0.0/24"]  # Ensure this subnet doesn't overlap with others
 }
 
 # Application Gateway
