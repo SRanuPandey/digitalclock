cfff291192a27d7092a05e4516529fded4016396
1988Shubham <shubhampandey88@gmail.com>
2024-10-13 20:30:24 +0100

    unique plan file - 2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_version = ">= 1.0"
 }
-provider "azurerm" {
-  features {}
-   resource_provider_registrations = "none"
-}
+#provider "azurerm" {
+#  features {}
+#   resource_provider_registrations = "none"
+#}
